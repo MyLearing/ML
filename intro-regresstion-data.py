53b9df895928e32d6e2112bc827b314beb2651c9
Teerapat Tee <teerapat12345678@gmail.com>
2018-08-28 21:51:31 +0700

    Regression Training and Testing

--- intro-regresstion-data.py
+++ intro-regresstion-data.py
@@ -22,7 +22,16 @@ forecast_out = int(math.ceil(0.1*len(df)))
 df['label'] = df[forecast_col].shift(-forecast_out)
 df.dropna(inplace=True)
 
-x = np.array(df.drop(['label'], 1))
+X = np.array(df.drop(['label'], 1))
 y = np.array(df['label'])
-x = preprocessing.scale(X)
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+
+print(accuracy)
 
