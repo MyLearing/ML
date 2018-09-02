8cee5d6263470336e9e01e85b5f431b15af65193
Teerapat Tee <teerapat12345678@gmail.com>
2018-09-02 14:32:43 +0700

    Train and test on the entire dataset overfitting

--- learning-models.py
+++ learning-models.py
@@ -2,6 +2,9 @@ from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+
 
 iris = load_iris()
 
@@ -14,8 +17,8 @@ logreg.fit(X, y)
 
 y_pred = logreg.predict(X)
 
-print(len(y_pred))
-print(metrics.accuracy_score(y, y_pred))
+# print(len(y_pred))
+# print(metrics.accuracy_score(y, y_pred))
 
 
 #KNN (K=5)
@@ -23,10 +26,52 @@ print(metrics.accuracy_score(y, y_pred))
 knn = KNeighborsClassifier(n_neighbors=5)
 knn.fit(X, y)
 y_pred = knn.predict(X)
-print(metrics.accuracy_score(y, y_pred))
+# print(metrics.accuracy_score(y, y_pred))
 
 #KNN (K=1)
 knn = KNeighborsClassifier(n_neighbors=1)
 knn.fit(X, y)
 y_pred = knn.predict(X)
-print(metrics.accuracy_score(y, y_pred))
+# print(metrics.accuracy_score(y, y_pred))
+
+print(X.shape)
+print(y.shape)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=4)
+
+print(X_train.shape)
+print(X_test.shape)
+
+print(y_train.shape)
+print(y_test.shape)
+
+logreg = LogisticRegression()
+logreg.fit(X_train, y_train)
+
+y_pred = logreg.predict(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train, y_train)
+y_pred = knn.predict(X_test)
+print(metrics.accuracy_score(y_test, y_pred))
+
+#
+k_range = list(range(1, 26))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(X_train, y_train)
+    y_pred = knn.predict(X_test)
+    scores.append(metrics.accuracy_score(y_test, y_pred))
+
+
+# %matplotlib inline
+
+plt.plot(k_range, scores)
+plt.xlabel('Value of K for KNN')
+plt.ylabel('Testing Accuracy')
